feca317f705f528bdfeccc1cf7776f2b74a34c84
Matthias Hirsch <8hirsch@informatik.uni-hamburg.de>
2025-04-24 17:32:57 +0200

    Add service

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+# Run server
+
+## Pip
+
+`pip install .`
+`fastapi dev main.py`
+
+## UV
+
+(install uv)
+`uv run fastapi dev`
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from service import generate_report
 
 app = FastAPI()
 
@@ -6,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/generate_report")
+async def call_service():
+    result = generate_report()
+    return {"result": result}
