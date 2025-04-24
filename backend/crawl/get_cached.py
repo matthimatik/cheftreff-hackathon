022251f3bb0dd13503b5e202b41fdcf914be55c5
TheMcs000 <TheMcs000@cloak.github.com>
2025-04-24 22:05:18 +0200

    Add reliefweb crawler

---
+++ backend/crawl/get_cached.py
@@ -0,0 +1,39 @@
+from settings import CRAWL_CACHE_DIR
+import json
+import requests
+
+
+def get_cached(url: str) -> str:
+    """
+    Retrieves data from a HTTP Endpoint.
+    If the file has been requested before, it will be loaded from the cache.
+
+    WARNING: This function is not thread-safe.
+    """
+    cache_path = CRAWL_CACHE_DIR / "cache.json"
+    if cache_path.exists():
+        with open(cache_path, "r") as f:
+            cache = json.load(f)
+    else:
+        cache = {}
+
+    cached_data_file = cache.get(url)
+    if cached_data_file is not None:
+        return (CRAWL_CACHE_DIR / cached_data_file).read_text()
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(f"Error: {response.status_code}")
+
+    data = response.text
+    cache_id = len(cache)
+    target_file_name = f"{cache_id}.txt"
+    cache[url] = target_file_name
+
+    with open(CRAWL_CACHE_DIR / target_file_name, "w") as f:
+        f.write(data)
+
+    with open(cache_path, "w") as f:
+        json.dump(cache, f)
+
+    return data
--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
+
 DATA_DIR = BASE_DIR / "data"
+
+CRAWL_CACHE_DIR = BASE_DIR / "crawl/cache"
